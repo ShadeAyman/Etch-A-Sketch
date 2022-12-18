d44a574b3aabdd1c0551864adb31353216be1aa4
miwon <ayman.shade99@gmail.com>
2022-12-18 19:49:58 +0200

    updated the script file

--- script.js
+++ script.js
@@ -3,40 +3,138 @@ const cell = document.createElement("div");
 cell.classList.add('cell');
 const slider = document.getElementById("slider");
 const sliderDisplay = document.getElementById("display");
-const removecell =document.getElementsByClassName('cell');
 const cellclass = document.getElementsByClassName("cell");
+const Rainbow = document.getElementById("Rainbow");
+const Black = document.getElementById("black");
+const Lighten = document.getElementById("lighten");
 const clearButton = document.getElementById("clear");
-
-//functions
-function gridbuilder (columns =slider.value , rows =slider.value) //a function that builds cells
-{
+const Toggle = document.getElementById("toggle");
+//defualt values
+let usedbursh = "Black";
+let isToggle = false;
+gridbuilder(slider.value, slider.value);
+//--------------functions--------------//
+//a function that builds cells
+function gridbuilder(columns = slider.value, rows = slider.value) {
     deletElements();
-    gridcontainer.style.gridTemplateColumns = " repeat("+columns+", 1fr)";
-    gridcontainer.style.gridTemplateRows = " repeat("+rows+", 1fr)";
-    let cellcount = columns*rows;
-    for (let i = 1; i <= cellcount; i++)
-    {
+    gridcontainer.style.gridTemplateColumns = " repeat(" + columns + ", 1fr)";
+    gridcontainer.style.gridTemplateRows = " repeat(" + rows + ", 1fr)";
+    let cellcount = columns * rows;
+    for (let i = 1; i <= cellcount; i++) {
         gridcontainer.appendChild(cell.cloneNode());
     }
+    brush(usedbursh)
 }
 
 function deletElements()//remove elments
 {
-    while(gridcontainer.lastElementChild)
-    {
+    while (gridcontainer.lastElementChild) {
         gridcontainer.removeChild(gridcontainer.lastElementChild)
     }
 }
 //-------------to show the slide value and update it
-sliderDisplay.innerHTML=slider.value+"x"+slider.value;
-slider.oninput = function() {
-    sliderDisplay.innerHTML = this.value+"x"+this.value;
+sliderDisplay.innerHTML = slider.value + "x" + slider.value;
+slider.oninput = function () {
+    sliderDisplay.innerHTML = this.value + "x" + this.value;
 };
 //-------------------------------------------------------
-gridbuilder (slider.value , slider.value)
-  slider.addEventListener('input',()=>gridbuilder ());
+function brush(brushtype) {
+    if (brushtype === "Rainbow") {
+        if (isToggle == true) {
+            for (const x of cellclass) {
+                x.addEventListener('mouseover', () => {
+                    x.style.backgroundColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+                    x.style.transition= "background-color 1.0s ease";
 
-    clearButton.addEventListener('click',
-    ()=>{deletElements();
-    gridbuilder();}
-    );
+                })
+            }
+        }
+        else {
+            for (const x of cellclass) {
+                x.addEventListener('mousedown', () => {
+                    x.style.backgroundColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+                    x.style.transition= "background-color 1.0s ease";
+
+                })
+            }
+        }
+    }
+    if (brushtype === "Black") {
+        if (isToggle == true) {
+            for (const x of cellclass) {
+                x.addEventListener('mouseover', () => {
+                    x.style.backgroundColor = "black";
+                    x.style.transition= "background-color 1.0s ease";
+
+                })
+            }
+        }
+        else {
+            for (const x of cellclass) {
+                x.addEventListener('mousedown', () => {
+                    x.style.backgroundColor = "black";
+                    x.style.transition= "background-color 1.0s ease";
+                })
+            }
+        }
+    }
+    if (brushtype === "Lighten") {
+        if (isToggle == true) {
+            for (const x of cellclass) {
+                x.addEventListener('mouseover', () => {
+                    x.style.backgroundColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+                })
+            }
+        }
+        else {
+            for (const x of cellclass) {
+                x.addEventListener('mousedown', () => {
+                    x.style.backgroundColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+                })
+            }
+        }
+    }
+}
+
+function random(number) {
+    return Math.floor(Math.random() * (number + 1));
+}
+
+
+function updateToggle()
+{
+    if(isToggle === false) {
+        isToggle = true;
+    }
+    else {
+        isToggle = false;
+    }
+    console.log(isToggle);
+    brush(usedbursh);
+}
+//----------------------Listeners-------------//
+slider.addEventListener('input', () => gridbuilder());
+
+clearButton.addEventListener('click', //to clear the tiles and redrow them
+    () => {
+        deletElements();
+        gridbuilder();
+    }
+);
+Toggle.addEventListener('click', updateToggle);
+
+Rainbow.addEventListener('click', () => {
+    usedbursh = "Rainbow";
+    brush(usedbursh)
+}
+)
+Black.addEventListener('click', () => {
+    usedbursh = "Black";
+    brush(usedbursh)
+}
+)
+Lighten.addEventListener('click', () => {
+    usedbursh = "Lighten";
+    brush(usedbursh)
+}
+)
